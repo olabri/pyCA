b0812092078b4e8484e11335fef4f62aca45618c
Anders Myren <anders.myren@uib.no>
2020-06-12 13:42:38 +0200

    Revert "Handling "file exists" errors"
    This reverts commit 1e439e4c9b28269817170a80d136a6e63e3cb4ca.

--- pyca/utils.py
+++ pyca/utils.py
@@ -105,21 +105,6 @@ def try_mkdir(directory):
         if err.errno != errno.EEXIST:
             raise err
 
-def try_mkdir_event(directory):
-    '''Try to create a directory. Rename and create new if it already exists.
-    '''
-    try:
-        os.mkdir(directory)
-    except OSError as err:
-        if err.errno == errno.EEXIST:
-            now = time.strftime("%Y%m%d%H%M%S",time.gmtime())
-            logger.warning('Event directory %s already exists, renaming it with timestamp %s' % (directory, now))
-            os.rename(directory, directory + "_" + now)
-            os.mkdir(directory)
-        else:
-            raise err
-
-
 
 def configure_service(service):
     '''Get the location of a given service from Opencast and add it to the
